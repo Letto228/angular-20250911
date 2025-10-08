dc55e90a8c7c29b886b4f3002a7f950bcf2bae67
Alexander <amv1017@mail.ru>
2025-10-08 18:05:29 +0300

    lecture 4: input, output for product card

--- src/app/pages/products-list/products-list.ts
+++ src/app/pages/products-list/products-list.ts
@@ -1,5 +1,7 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {signal, ChangeDetectionStrategy, Component, computed} from '@angular/core';
 import {Card} from './card/card';
+import {Product} from '../../shared/products/product.type';
+import {productsMock} from '../../shared/products/products.mock';
 
 @Component({
     selector: 'app-products-list',
@@ -8,4 +10,13 @@ import {Card} from './card/card';
     styleUrl: './products-list.css',
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ProductsList {}
+export class ProductsList {
+    index = signal<number>(0);
+    maxIndex = productsMock.length;
+
+    product = computed(() => productsMock[this.index()]);
+
+    onBuyEvent(product: Product) {
+        confirm(`Добавить ${product.name} в корзину?`);
+    }
+}
