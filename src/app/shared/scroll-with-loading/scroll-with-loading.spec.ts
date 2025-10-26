58099a14915c4ece5f96c708ff1936005ccc0cdf
Ketrosan Gleb <ketrosan.ga@devzone.local>
2025-10-26 12:39:51 +0300

    Feat: directive scroll with loading

---
+++ src/app/shared/scroll-with-loading/scroll-with-loading.spec.ts
@@ -0,0 +1,8 @@
+import {ScrollWithLoading} from './scroll-with-loading';
+
+describe('ScrollWithLoading', () => {
+    it('should create an instance', () => {
+        const directive = new ScrollWithLoading();
+        expect(directive).toBeTruthy();
+    });
+});
